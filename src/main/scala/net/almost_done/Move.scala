67e9538b1351811b84ce07cfc4a297e80180b75e
Jacek Królikowski <nietaki@gmail.com>
2014-07-23 20:28:10 +0200

    Move definitions

---
+++ src/main/scala/net/almost_done/Move.scala
@@ -0,0 +1,21 @@
+package net.almost_done
+
+/**
+ * Created by nietaki on 23.07.14.
+ */
+sealed trait Move {
+
+}
+
+/**
+ * the move where the current player plays one or more cards of the same rank
+ * @param card the card rank
+ * @param count how many cards are played
+ */
+case class Play(card: Int, count: Int) extends Move
+
+/**
+ * the move where the current player draws one or more cards from the stack
+ * @param count how many cards are drawed
+ */
+case class Draw(count: Int) extends Move
